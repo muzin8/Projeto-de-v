2a638a70ed63ea5e87d84bbefd40d213018a98e6
muzin8 <murilo.hoffmann.machado@escola.pr.gov.br>
2026-06-08 11:04:08 +0000

    aula 4

--- main.js
+++ main.js
@@ -11,10 +11,25 @@ document.addEventListener("DOMContentLoaded", () => {
     if (botoes[index]) botoes[index].classList.add("ativo");
     if (abas[index]) abas[index].classList.add("ativo");
   };
-
   botoes.forEach((botao, index) => {
     botao.addEventListener("click", () => ativarAba(index));
   });
 
   ativarAba(0);
 });
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+  };
+}
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
